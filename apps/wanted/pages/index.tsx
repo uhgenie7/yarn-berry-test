23de3c1c1e2edfc1f6fd3c3a5ba7e750b6d05a42
HyejinUm <uhj1993@naver.com>
2022-12-13 19:46:44 +0900

    test: deploy #1

--- apps/wanted/pages/index.tsx
+++ apps/wanted/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js</a>
         </h1>
         <h2>{sayHello()}</h2>
         {/* <Button variant="contained">Hello From @wanted/ui</Button> */}
